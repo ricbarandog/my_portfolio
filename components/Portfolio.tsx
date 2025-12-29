8fe65096d2939b902e5eec0b3d9c70c5494c97b6
Ricardo Barandog Jr. <118973778+ricbarandog@users.noreply.github.com>
2025-12-29 15:45:13 +0800

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -8,7 +8,7 @@ const projects: ProjectLink[] = [
     title: "Booking System",
     category: "coding",
     url: "https://pickle-booking.vercel.app/", 
-    thumbnail: ".components/enrollment.PNG", 
+    thumbnail: "./booking.png", 
     description: "A secure booking system for Pickle Ball business.",
     tags: ["React", "Security", "Portal"]
   },
