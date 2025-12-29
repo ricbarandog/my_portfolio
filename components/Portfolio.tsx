58c4fb5a6c6cc9bc138e5b0e1f398747e80bbe00
Ricardo Barandog Jr. <118973778+ricbarandog@users.noreply.github.com>
2025-12-29 21:05:28 +0800

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -61,7 +61,7 @@ export const Portfolio: React.FC = () => {
             Featured Projects
           </h2>
           <p className="max-w-2xl mx-auto text-slate-500 text-lg leading-relaxed">
-            Bridging the gap between code and creativity. Explore my technical builds and creative productions.
+            Bridging the gap between code and creativity. Explore my technical builds and creative productions using AI.
           </p>
 
           {/* Filtering Tabs */}
